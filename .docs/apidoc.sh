60aecb13f35cc876a4e0069a4ff961e35c6c936b
vsoch <vsochat@stanford.edu>
2018-02-11 16:18:13 -0800

    adding missing modules

--- .docs/apidoc.sh
+++ .docs/apidoc.sh
@@ -4,13 +4,25 @@
 #
 # sphinx-apidoc -o source/ ../spython
 
+function rename_folder() {
+    name=$1
+
+}
+
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE=$(dirname $HERE)
 cd $HERE
 cd $BASE && python setup.py install && cd $HERE && make html
 rm -rf $BASE/docs/api
+
+# Create new folders
 mkdir -p $BASE/docs/api
-mkdir -p $BASE/docs/api/assets
+
+# Rename folders
 find $HERE/_build/html -exec sed -i -e 's/_static/assets/g' {} \;
-cp -R $HERE/_build/html/_static/* $HERE/_build/html/assets
+find $HERE/_build/html -exec sed -i -e 's/_modules/modules/g' {} \;
+
+# Copy to new locations
+mv $HERE/_build/html/_static $BASE/docs/api/assets
+mv $HERE/_build/html/_modules $BASE/docs/api/modules
 cp -R $HERE/_build/html/* $BASE/docs/api
