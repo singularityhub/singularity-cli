a14ffa1acb6f9046b17c0e623e3bf7679f859f7d
Vanessa Sochat <vsochat@stanford.edu>
2018-04-25 17:54:03 -0400

    updating headers

--- spython/tests/test_utils.py
+++ spython/tests/test_utils.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
 
-'''
+# Copyright (C) 2017-2018 Vanessa Sochat.
+# Copyright (C) 2018 The Board of Trustees of the Leland Stanford Junior
+# University.
 
-Copyright (C) 2017 The Board of Trustees of the Leland Stanford Junior
-University.
-Copyright (C) 2016-2017 Vanessa Sochat.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
-License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-'''
 from singularity.utils import get_installdir
 from numpy.testing import (
     assert_array_equal, 
