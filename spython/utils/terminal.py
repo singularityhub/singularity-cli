9ed39433d9c381697f1f680ea2f310091e0b00f0
Jon Clucas <jon@vasegurt.com>
2020-05-21 17:03:15 -0400

    :children_crossing: Add stderr to generator when streaming

--- spython/utils/terminal.py
+++ spython/utils/terminal.py
@@ -134,7 +134,12 @@ def stream_command(cmd, no_newline_regexp="Progess", sudo=False, sudo_options=No
     """
     cmd = _process_sudo_cmd(cmd, sudo, sudo_options)
 
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True
+    )
     for line in iter(process.stdout.readline, ""):
         if not re.search(no_newline_regexp, line):
             yield line
