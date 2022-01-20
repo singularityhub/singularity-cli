bd53e5ca6fd875f33135a35bbf761f43f1144c1a
vsoch <vsoch@users.noreply.github.com>
2022-01-19 19:29:21 -0700

    works locally?
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- spython/tests/test_client.py
+++ spython/tests/test_client.py
@@ -35,16 +35,14 @@ def test_export():
 def test_docker_pull(docker_container):
     tmp_path, container = docker_container
     print(container)
-    ext = "sif" if Client.version_info().major >= 3 else "simg"
-    assert container == str(tmp_path / ("busybox:1.30.1." + ext))
+    assert container == str(tmp_path / ("busybox:1.30.1.sif"))
     assert os.path.exists(container)
 
 
 def test_oras_pull(oras_container):
     tmp_path, container = oras_container
     print(container)
-    ext = "sif" if Client.version_info().major >= 3 else "simg"
-    assert container == str(tmp_path / ("github-ci:latest." + ext))
+    assert container == str(tmp_path / ("github-ci:latest.sif"))
     assert os.path.exists(container)
 
 
